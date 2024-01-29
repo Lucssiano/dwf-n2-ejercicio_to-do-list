20765e95f257051cae3f8a63558aa2d9566be374
Luciano Riente <lucianotomasriente@gmail.com>
2024-01-28 21:30:37 -0300

    Creacion del componente del formulario que agrega items a la lista

--- dist/index.b71e74eb.js
+++ dist/index.b71e74eb.js
@@ -579,8 +579,68 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"h7u1C":[function(require,module,exports) {
+var _addItemForm = require("./components/add-item-form");
+(function main() {
+    // initList();
+    (0, _addItemForm.initForm)();
+})();
+
+},{"./components/add-item-form":"74gDE"}],"74gDE":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "initForm", ()=>initForm);
+function initForm() {
+    class ItemForm extends HTMLElement {
+        constructor(){
+            super();
+            this.render();
+        }
+        render() {
+            const shadow = this.attachShadow({
+                mode: "open"
+            });
+            const form = document.createElement("form");
+            form.classList.add("to-do-list-form");
+            form.innerHTML = `
+      <form class="to-do-list-form">
+        <input type="text" name="item" placeholder="Agreg\xe1 un item" class="form__input"/>
+        <button type="submit" class="form__button">+</button>
+      </form>
+      `;
+            const style = document.createElement("style");
+            style.innerHTML = `
+      .to-do-list-form {
+        display: flex;
+        justify-content: space-between;
+        gap: 10px;
+        height: 100%;
+      }
+      .form__input {
+        min-width: 130px;
+        width: 80%;
+        padding: 20px;
+      }
+      .form__button {
+        min-width: 60px;
+        font-size: 30px;
+      }
+      @media (min-width: 769px) {
+        .form__button {
+          cursor: pointer;
+        }
+      }
+      `;
+            const formEl = form.querySelector(".to-do-list-form");
+            formEl?.addEventListener("click", (event)=>{
+                event.preventDefault();
+            // agregar funcionalidad
+            });
+            shadow.appendChild(form);
+            shadow.appendChild(style);
+        }
+    }
+    customElements.define("add-item-form", ItemForm);
+}
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports) {
 exports.interopDefault = function(a) {
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 import { state } from './state';
+import { initForm } from './components/add-item-form';
+
+(function main() {
+	// initList();
+	initForm();
+})();
